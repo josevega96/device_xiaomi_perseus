47282530cd0f7e51fbe038d3397c50d22db80f9d
josevega96 <jdelavega36@gmail.com>
2020-09-11 13:06:31 -0500

    perseus: sign AVB image
    non AB AVB images must now be signed to compile

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2019-2020 The LineageOS Project
+ # Copyright (C) 2019-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -15,7 +14,7 @@ TARGET_OTA_ASSERT_DEVICE := perseus
 # Display
 TARGET_SCREEN_DENSITY := 440
 
-# Kernel
+# Kernel 
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := perseus_defconfig
 
@@ -38,6 +37,10 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 TARGET_USES_NQ_NFC := true
 
 # Verified Boot
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048 
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
