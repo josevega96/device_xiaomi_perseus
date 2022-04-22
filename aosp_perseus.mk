ea475dfad1d6543b82b611c8dd8f8f1af7b57585
josevega96 <jdelavega36@gmail.com>
2022-04-22 13:42:31 -0500

    Perseus: PE-ify

--- aosp_perseus.mk
+++ aosp_perseus.mk
@@ -5,12 +5,14 @@
 #
 
 $(call inherit-product, device/xiaomi/perseus/device.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_perseus
+PRODUCT_NAME := aosp_perseus
 PRODUCT_DEVICE := perseus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 3
